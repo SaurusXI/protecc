08c0bbe6e7ef7e689fa40d322c9ea4ccdb948f31
SaurusXI <vermashantanu@hotmail.com>
2020-10-27 13:39:07 +0530

    modify filter

--- src/filter/filter.go
+++ src/filter/filter.go
@@ -1,30 +1,16 @@
 package filter
 
 import (
-	"fmt"
 	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	// "github.com/SaurusXI/filter/ipfilter"
+	"github.com/SaurusXI/protecc/src/filter/ipfilter"
+	"github.com/SaurusXI/protecc/src/filter/tcpfilter"
+	"github.com/SaurusXI/protecc/src/config"
 )
 
-func getPacketInfo(packet gopacket.Packet) {
-	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
-		fmt.Println("IP Layer found")
-		ip, _ := ipLayer.(*layers.IPv4)
+type Filter struct {}
 
-		fmt.Println("From ", ip.SrcIP, " to ", ip.DstIP)
-		fmt.Println("Protocol: ", ip.Protocol)
-	}
-
-	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-		fmt.Println("TCP Layer found")
-		tcp, _ := tcpLayer.(*layers.TCP)
-
-		fmt.Println("From port ", tcp.SrcPort, " to ", tcp.DstPort)
-		fmt.Println("Sequence number: ", tcp.Seq)
-	}
-
-	if err := packet.ErrorLayer(); err != nil {
-		fmt.Println("Error decoding packet:", err)
-	}
+func (f Filter) Allowed(c config.Config, packet gopacket.Packet) bool {
+	ipAllowed, _ := ipfilter.Allowed(c, packet) 
+	tcpAllowed, _ := tcpfilter.Allowed(c, packet)
+	return ipAllowed && tcpAllowed
 }
