7a6c73aec07423ae652b82f76ff105ad937ed366
SaurusXI <vermashantanu@hotmail.com>
2020-10-27 13:37:48 +0530

    add config

--- src/config/config.go
+++ src/config/config.go
@@ -2,8 +2,18 @@ package config
 
 import (
 	"github.com/SaurusXI/protecc/src/config/src_ip"
+	"github.com/SaurusXI/protecc/src/config/dest_ip"
+	"github.com/SaurusXI/protecc/src/config/src_port"
+	"github.com/SaurusXI/protecc/src/config/dest_port"
+	"github.com/SaurusXI/protecc/src/config/window_tcp"
+	"github.com/SaurusXI/protecc/src/config/checksum_tcp"
 )
 
 type Config struct {
-	ipsrc src_ip.Config
+	IPsrc 		src_ip.Config
+	IPdest 		dest_ip.Config
+	Portsrc 	src_port.Config
+	Portdest 	dest_port.Config
+	Window 		window_tcp.Config
+	Checksum 	checksum_tcp.Config
 }
