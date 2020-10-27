884d4a950b82cd37d431ad5dc536c506d6d99177
SaurusXI <vermashantanu@hotmail.com>
2020-10-27 13:38:15 +0530

    modify ipfilter

--- src/filter/ipfilter/ipfilter.go
+++ src/filter/ipfilter/ipfilter.go
@@ -1,9 +1,34 @@
 package ipfilter
 
-// import (
-// 	"fmt"
-// 	"github.com/google/gopacket"
-// 	"github.com/google/gopacket/layers"
-// )
+import (
+	"github.com/SaurusXI/protecc/src/config"
+	"github.com/SaurusXI/protecc/src/gate"
+	"errors"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"net"
+)
 
-// Implement IP filtration logic
+func Allowed(c config.Config, packet gopacket.Packet) (bool, error) {
+	
+	if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+		// fmt.Println("IP Layer found")
+		ip, _ := ipLayer.(*layers.IPv4)
+		blocked := false
+
+		if c.IPsrc.Setting == gate.CLOSE {
+			blocked = c.IPsrc.Blocked[key(ip.SrcIP)] || blocked
+		}
+		if c.IPdest.Setting == gate.CLOSE {
+			blocked = c.IPdest.Blocked[key(ip.DstIP)] || blocked
+		}
+
+		return !blocked, nil
+	}
+
+	return false, errors.New("IP Layer not found")
+}
+
+func key(ip net.IP) string {
+	return string(ip.To16())
+}
